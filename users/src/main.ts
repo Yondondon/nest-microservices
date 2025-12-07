9d4bdab3690e1f9f839c28e946243dfe7d42982c
Yondondon <roman17091993@gmail.com>
2025-12-07 17:21:42 +0200

    infrastructure updates, migrated to pnpm

--- users/src/main.ts
+++ users/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { UsersModule } from './users.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const users = await NestFactory.create(UsersModule, {
+    logger: ['error', 'warn'],
+  });
+  await users.listen(process.env.PORT ?? 3001);
 }
 void bootstrap();
