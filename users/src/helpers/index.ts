a010ad52eae3edbdf93fcd39de15bdd8da2018f5
Yondondon <roman17091993@gmail.com>
2025-12-16 00:51:55 +0200

    improved communication between user microservice and gateway

--- users/src/helpers/index.ts
+++ users/src/helpers/index.ts
@@ -1,4 +1,1 @@
 export * from './map-user-entity-to-interface.helper';
-export * from './build-success-response.helper';
-export * from './build-error-response.helper';
-export * from './handle-error.helper';
--- users/src/users.interface.ts
+++ users/src/users.interface.ts
@@ -1,11 +1,3 @@
-import { HttpStatus } from '@nestjs/common';
-
-export interface IGenericResponse<T> {
-  status: HttpStatus;
-  data: T;
-  message?: string;
-}
-
 export interface IUser {
   username: string;
   password: string;
